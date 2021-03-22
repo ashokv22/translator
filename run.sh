2d1550751ef746f9d8a4473c982084d0c95de2c9
venkatarajv <venkatrajv143@gmail.com>
2021-03-22 18:49:46 +0530

    colab commit2

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ pip install easynmt
 echo "---------------Installing flask-ngrok------------------"
 pip install flask-ngrok
 echo "---------------RUN main.py------------------"
-python main.py
+#python main.py
 echo "---------------RUN app.py------------------"
-python app.py >> /dev/null &
+python $(pwd)/app.py >> /dev/null &
 
