3b78d4386b4ec0c210d99231e7d9e1d5733e302d
ashokv22 <80625802+ashokv22@users.noreply.github.com>
2021-05-12 12:19:04 +0530

    added_gtts

--- run.sh
+++ run.sh
@@ -13,6 +13,8 @@ pip3 install pycld2
 pip3 install morfessor
 echo "---------------Installing flask-ngrok------------------"
 pip install flask-ngrok
+pip install gTTS
+pip install playsound
 echo "---------------RUN main.py------------------"
 #python main.py
 echo "---------------RUN app.py------------------"
